6a2cb1e2cd0240976716f24bac23720dc82dc89b
ProgrammerJayT <theanthem8@gmail.com>
2022-06-19 15:19:14 +0200

    Initialized Guzzle HTTP request to remove background from image

--- private/controllers/Home.php
+++ private/controllers/Home.php
@@ -1,9 +1,65 @@
 <?php
 
-
 class Home extends Controller
 {
     function index(){
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $supportedExtension = array('jpg', 'jpeg', 'png');
+
+            $imageExtension = explode('/', $_FILES['image']['type']);
+            $imageExtension = $imageExtension[1];
+
+            $path = $_FILES['image']['tmp_name'];
+
+            if (!in_array($imageExtension, $supportedExtension)) {
+                echo '<script>alert("File type not supported");</script>';
+            } else {
+
+                $results = array();
+
+                $client = new GuzzleHttp\Client();
+                $res = $client->post('https://api.remove.bg/v1.0/removebg', [
+                    'multipart' => [
+                        [
+                            'name'     => 'image_file',
+                            'contents' => fopen($path, 'r')
+                        ],
+                        [
+                            'name'     => 'size',
+                            'contents' => 'auto'
+                        ]
+                    ],
+                    'headers' => [
+                        'X-Api-Key' => '6JFSkLd54ZeGXesVWp52qu5i'
+                    ]
+                ]);
+                
+                if (!file_exists('output')) {
+                    mkdir('removed-bg', 0777, true);
+                }
+                $fp = fopen("removed-bg/removed-bg.png", "wb");
+                fwrite($fp, $res->getBody());
+                fclose($fp);
+
+                if ($res->getStatusCode() == 200) {
+                    $results['res'] = 'success';
+                    $results['message'] = 'Image background successfully removed';
+                    $results['imageURI'] = 'removed-bg/removed-bg.png';
+
+                    $_SESSION['return'] = $results;
+                    $this->redirect('Output');
+
+                } else {
+                    $results['res'] = 'error';
+                    $results['message'] = 'Error removing background';
+
+                    $_SESSION['return'] = $results;
+                    $this->redirect('Home');
+                }
+            }
+        }
         
         $this->view('home');
     }
